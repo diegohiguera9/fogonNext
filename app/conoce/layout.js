e1ce096c48ca86834a573481d13920a624f19068
Diego <drhiguerag@unal.edu.co>
2023-09-09 10:10:51 -0500

    inter class

--- app/conoce/layout.js
+++ app/conoce/layout.js
@@ -1,7 +1,7 @@
 export default async function RootLayout({ children }) {
   return (
     <div lang="es">
-      <div className={inter.className}>{children}</div>
+      <div>{children}</div>
     </div>
   )
 }

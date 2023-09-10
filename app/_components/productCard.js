464dcad2659e3ae35bcabf1a7dbc7dbfdf526e39
Diego <drhiguerag@unal.edu.co>
2023-09-10 18:05:02 -0500

    width

--- app/_components/productCard.js
+++ app/_components/productCard.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function ProductCard({ src, price, description, name }) {
   return (
     <>
-      <div className="flex justify-start aspect-[21/9] col-span-5 mb-4 sm:max-w-md sm:w-72 grow">
+      <div className="flex justify-start max-h-fit col-span-5 mb-4 max-w-full sm:max-w-md sm:w-96 grow">
         <div className="relative flex flex-col justify-start w-48 md:w-80 aspect-video rounded-lg overflow-hidden">
           <Image
             className="object-cover"

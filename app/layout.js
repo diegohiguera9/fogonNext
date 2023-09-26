ce3021fa3c3784b095ec2b5d395430a2d9232ccf
Diego <drhiguerag@unal.edu.co>
2023-09-26 11:06:21 -0500

    favicon

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,22 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import connect from '@/lib/mongoose'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import connect from "@/lib/mongoose";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'El Fogon Llanero',
-  description: 'El fogon llanero restaurante de carnes en bogota',
-}
+  title: "El Fogon Llanero",
+  description: "El fogon llanero restaurante de carnes en bogota",
+};
 
 export default async function RootLayout({ children }) {
-  await connect()
+  await connect();
   return (
     <html lang="es">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
